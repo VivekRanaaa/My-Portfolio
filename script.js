1d3f07f2b74174e2846d068b5fec6eada28427c9
Vivek rana <navurana433@gmail.com>
2024-10-24 14:19:05 +0530

    update

--- script.js
+++ script.js
@@ -28,3 +28,53 @@ hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
 //header-js-end//
+//experience-slider//
+let currentCustomIndex = 0;
+
+function showCustomSlide(index) {
+  const items = document.querySelectorAll(".custom-slider-item");
+  const totalItems = items.length;
+
+  if (index >= totalItems) {
+    currentCustomIndex = 0;
+  } else if (index < 0) {
+    currentCustomIndex = totalItems - 1;
+  } else {
+    currentCustomIndex = index;
+  }
+
+  document.getElementById("customSliderTrack").style.transform =
+    "translateX(" + -currentCustomIndex * 100 + "%)";
+}
+
+function nextCustomSlide() {
+  showCustomSlide(currentCustomIndex + 1);
+}
+
+function prevCustomSlide() {
+  showCustomSlide(currentCustomIndex - 1);
+}
+
+// Initialize the slider
+showCustomSlide(currentCustomIndex);
+//experience-slider-end//
+//BACK TO TOP//
+// Back to Top Button
+const backToTopButton = document.getElementById("backToTop");
+
+// Show or hide the button depending on scroll position
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "flex";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+// Scroll to top when the button is clicked
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
